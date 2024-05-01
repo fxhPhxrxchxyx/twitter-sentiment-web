c3a7c411969ef4f58c61bb2f8188abae426176d1
sittichok Ouamsiri <sutichok46@gmail.com>
2024-05-02 02:40:20 +0700

    feat: interface

--- frontend/src/getTweet.service.ts
+++ frontend/src/getTweet.service.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 
+export interface IGetTweetServiceResult {
+    result: {
+      brand: string;
+        sentiment: string;
+        examples: {
+            sentiment: string;
+            text: string;
+            from: string;
+        }[]
+    }[]
+}
+
 export const getTweet = async (text: string) => {
-  const resp = await axios.post(
+  const resp = await axios.post<IGetTweetServiceResult>(
     "https://api.senti-tweet.thistine.com/sentiment",
     {
       tweet: text,
